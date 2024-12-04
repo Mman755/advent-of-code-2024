3ec88f604fc211fce74c00629144c77da3989e8b
mman755 <mman755@aucklanduni.ac.nz>
2024-12-04 19:10:38 +1300

    Clean day 4

--- day04/part2.py
+++ day04/part2.py
@@ -15,17 +15,17 @@ for row in range(R):
     for col in range(C):
         first, second = "", ""
         r, c = row, col
+        r1, c1 = row, col + 2
         for i in range(3):
             if 0 <= r < R and 0 <= c < C:
                 first += data[r][c]
                 r += 1
                 c += 1
-        r, c = row, col + 2
-        for i in range(3):
-            if 0 <= r < R and 0 <= c < C:
-                second += data[r][c]
-                r += 1
-                c += -1
+            if 0 <= r1 < R and 0 <= c1 < C:
+                second += data[r1][c1]
+                r1 += 1
+                c1 += -1
+
         if (first == search or first[::-1] == search) and (second == search or second[::-1] ==
                                                            search):
             ans += 1
