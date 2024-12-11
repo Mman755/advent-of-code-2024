29a7142780de9190ee519633cfbc0329f8cd897c
mman755 <mman755@aucklanduni.ac.nz>
2024-12-11 21:05:21 +1300

    Day 11

---
+++ day11/part2.py
@@ -0,0 +1,23 @@
+import sys
+from functools import lru_cache
+
+@lru_cache(None)
+def blink(stone, iters):
+    if iters == 0:
+        return 1
+
+    if stone == 0:
+        return blink(1, iters - 1)
+
+    elif len(str(stone)) % 2 == 0:
+        length = len(str(stone))
+        first, second = int(str(stone)[:length // 2]), int(str(stone)[length // 2:])
+        return blink(first, iters - 1) + blink(second, iters - 1)
+
+    return blink(stone * 2024, iters - 1)
+
+def solve(stones, iterations):
+    return sum([blink(stone, iterations) for stone in stones])
+
+stones = [int(stone) for stone in sys.stdin.readline().split(" ")]
+print(solve(stones, 75))
